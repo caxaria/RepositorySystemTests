2549c7a84e9d188cd61e6a43b0c8f6eca07681d1
Nuno Teixeira <nuno@codacy.com>
2014-05-29 19:11:39 +0100

    initial commit

---
+++ test.js
@@ -0,0 +1,47 @@
+(function() {
+  var ClassVariable;
+
+  ClassVariable = (function() {
+
+    var fibonacciCache = [];
+
+    function Fibonacci() {
+      fibonacciCache[0] = 0;
+      fibonacciCache[1] = 1;
+    }
+
+    Fibonacci.prototype.fibonacci = function(n) {
+      var current = 2;
+
+      while (fibonacciCache[n] === undefined) {
+        fibonacciCache[current] = fibonacciCache[current - 1] + fibonacciCache[current - 2];
+        fibonacciRecursive(current++);
+      }
+
+      console.log("Fibonnacci[%s] = %s", n, fibonacciCache[n]);
+
+      return fibonacciCache[n];
+    };
+
+    function fibonacciRecursive(current) {
+      fibonacciCache[current] = fibonacciCache[current - 1] + fibonacciCache[current - 2];
+    }
+
+    var instance;
+    return {
+      getInstance: function() {
+        if (instance === undefined) {
+          instance = new Fibonacci();
+          instance.constructor = null;
+        }
+        return instance;
+      }
+    };
+
+  })();
+
+  Fibonacci = ClassVariable;
+  return Fibonacci;
+
+})();
+
